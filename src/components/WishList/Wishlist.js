101cb8918944bee7e4b80a78bccbf05e2f56492b
safasuli01 <safa.abduallah2001@hotmail.com>
2024-08-15 14:16:00 +0300

    whishlist jsx

--- src/components/WishList/Wishlist.js
+++ src/components/WishList/Wishlist.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import MovieCard from '../MovieCard/MovieCard';
 import './Wishlist.css';
 
-function Wishlist({ wishlist }) {
+function Wishlist({ wishlist, onFavoriteToggle }) {
   return (
     <div className="wishlist">
       <h2>My Wishlist</h2>
-      {wishlist.length === 0 ? (
-        <p>No movies in your wishlist</p>
-      ) : (
-        wishlist.map(movie => (
-          <div key={movie.id} className="wishlist-item">
-            <img src={movie.posterUrl} alt={movie.title} className="wishlist-poster" />
-            <div className="wishlist-info">
-              <h5>{movie.title}</h5>
-              <p>{movie.releaseDate}</p>
-            </div>
-          </div>
-        ))
-      )}
+      <div className="row">
+        {wishlist.length === 0 ? (
+          <p>No movies in your wishlist</p>
+        ) : (
+          wishlist.map(movie => (
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              onFavoriteToggle={onFavoriteToggle}
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 }
